771ab3642ed299d258005cfa5f37cf3ec0d73539
tarun <raikwartarun72@gmail.com>
2023-06-11 18:36:00 +0530

    set time

--- server.js
+++ server.js
@@ -21,15 +21,7 @@ const FieldAgent = mongoose.model("FieldAgent")
 
 
 const getCurrentDate = ()=>{
-    
-    var date = new Date().getDate();
-    var month = new Date().getMonth() + 1;
-    var year = new Date().getFullYear();
-    
-    //Alert.alert(date + '-' + month + '-' + year);
-    // You can turn it in to your desired format
-    return date + '-' + month + '-' + year;//format: d-m-y;
-    
+    return new Date();
 }
 
 // *********************** get requests *************************
